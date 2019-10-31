14fda83decac1524dbc303f6cdcdc2fc6a4ea019
unknown <l.prystansky@gmail.com>
2019-10-31 18:08:30 +0200

    add one-page-scroll template

---
+++ ops/src/assets/js/script.js
@@ -0,0 +1,25 @@
+TweenLite.defaultEase = Linear.easeNone;
+var titles = document.querySelectorAll(".section-main");
+var controller = new ScrollMagic.Controller();
+var tl = new TimelineMax();
+
+// create timeline
+// this could also be created in a loop
+tl.to("#js-slideContainer", 1, {xPercent: -20}, "label1");
+tl.from(titles[1], 0.5, {opacity:0}, "label1+=0.5");
+tl.to("#js-slideContainer", 1, {xPercent: -40}, "label2");
+tl.from(titles[2], 0.5, {opacity:0}, "label2+=0.5");
+tl.to("#js-slideContainer", 1, {xPercent: -60}, "label3");
+tl.from(titles[3], 0.5, {opacity:0}, "label3+=0.5");
+tl.to("#js-slideContainer", 1, {xPercent: -80}, "label4");
+tl.from(titles[4], 0.5, {opacity:0}, "label4+=0.5");
+
+new ScrollMagic.Scene({
+  triggerElement: "#js-wrapper",
+  triggerHook: "onLeave",
+  duration: "400%"
+})
+  .setPin("#js-wrapper")
+  .setTween(tl)
+
+  .addTo(controller);
